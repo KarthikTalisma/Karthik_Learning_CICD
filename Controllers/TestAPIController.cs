2ac18208f4786efa6ff4aab7def94a8e64578c41
KarthikTalisma <karthikeyans@talisma.com>
2025-09-29 01:05:24 +0530

    Initial commit

---
+++ Controllers/TestAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestCICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestAPIController : ControllerBase
+    {
+        [HttpGet("get")]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Test GET API is working!" });
+        }
+
+        [HttpPost("post")]
+        public IActionResult Post()
+        {
+            return Ok(new { message = "Test POST API is working!" });
+        }
+    }
+}
---
+++ Controllers/TestMVCController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCICD.Controllers
+{
+    public class TestMVCController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
